e751a7ec8779bb571d4e1f05bcc8055b4349187a
Jisan Zaman <jzaman@wesleyan.edu>
2015-03-31 23:31:29 -0400

    accounted for network is available or not

--- app/src/main/java/superjisan/com/stormy/MainActivity.java
+++ app/src/main/java/superjisan/com/stormy/MainActivity.java
@@ -1,10 +1,15 @@
 package superjisan.com.stormy;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 import com.squareup.okhttp.Call;
 import com.squareup.okhttp.Callback;
@@ -26,32 +31,53 @@ public class MainActivity extends Activity {
         double longitude = -122.423 ;
         String forecasturl  = "https://api.forecast.io/forecast/"+apiKey+"/"+
                 latitude+","+longitude;
+         if(isNetworkAvailable()) {
+             OkHttpClient client = new OkHttpClient();
+             Request request = new Request.Builder()
+                     .url(forecasturl)
+                     .build();
 
-        OkHttpClient client = new OkHttpClient();
-        Request request = new Request.Builder()
-                .url(forecasturl)
-                .build();
+             Call call = client.newCall(request);
+             call.enqueue(new Callback() {
+                 @Override
+                 public void onFailure(Request request, IOException e) {
+                     alertUserAboutError();
+                 }
 
-        Call call = client.newCall(request);
-        call.enqueue(new Callback() {
-            @Override
-            public void onFailure(Request request, IOException e) {
+                 @Override
+                 public void onResponse(Response response) throws IOException {
+                     try {
+                         Log.v(TAG, response.body().string());
+                         if (response.isSuccessful()) {
 
-            }
+                         } else {
+                             alertUserAboutError();
+                         }
+                     } catch (IOException e) {
+                         Log.e(TAG, "Exception caught", e);
+                     }
+                 }
+             });
 
-            @Override
-            public void onResponse(Response response) throws IOException {
-                try {
-                    if(response.isSuccessful()){
-                        Log.v(TAG, response.body().string());
-                    }
-                } catch (IOException e) {
-                    Log.e(TAG, "Exception caught", e);
-                }
-            }
-        });
+         }else{
+             Toast.makeText(this, getString(R.string.network_unavailable_message), Toast.LENGTH_LONG).show();
+         }
+    }
 
+    private boolean isNetworkAvailable() {
+        ConnectivityManager manager = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = manager.getActiveNetworkInfo();
+        boolean isAvailable = false;
+        if(networkInfo != null && networkInfo.isConnected()){
+            isAvailable = true;
+        }
 
+        return isAvailable;
+    }
+
+    private void alertUserAboutError() {
+        AlertDialogFragment dialog = new AlertDialogFragment();
+        dialog.show(getFragmentManager(), "error_dialog");
     }
 
 }
